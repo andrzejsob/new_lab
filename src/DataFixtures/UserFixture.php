5b9d2c525a7adc85fbf9ad9f9c50c32bf04d60f0
andrew <andreeww2@gmail.com>
2019-09-20 00:41:22 +0200

    User login

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class UserFixture extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $user = new User();
+        $user->setEmail('andreeww2@gmail.com');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'gainward'
+        ));
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
